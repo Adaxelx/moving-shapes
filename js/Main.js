e565ed7c3bd33e28053bb4c2119ef1f93a5ff732
Adrian Mostowski <adaxelx@o2.pl>
2019-04-12 23:49:45 +0200

    add new shape to js

--- js/Main.js
+++ js/Main.js
@@ -1,12 +1,18 @@
 const square = document.querySelector('.square');
 const circle = document.querySelector('.circle');
 const rect3 = document.querySelector('.rect3')
+const rect2 = document.querySelector('.rect2')
 let scrollValue;
 const handleScroll = () =>{
     scrollValue = window.scrollY;
     square.style.left = `${ -50-scrollValue/10}%`
     circle.style.left = `${20+scrollValue/3}%`
     rect3.style.left = `${80+scrollValue/3}%`
+    if(scrollValue<200){
+        rect2.style.position = 'absolute';
+    }else if(scrollValue>=200){
+        rect2.style.position = 'fixed';
+    }
 }
 
 
